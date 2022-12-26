2923e3189c53d6b60244923ef526d9cc050e254a
BiwThRps <biw.thrps@gmail.com>
2022-12-26 20:17:14 +0700

    3729

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -4,8 +4,8 @@ export const actions: Actions = {
   login: async ({ cookies, request }) => {
     const data = await request.formData();
     const linAutURL = 'https://access.line.me/oauth2/v2.1/authorize'
-    const responseType = 'code', clientId = 1657770559, redirect_uri=''
-    console.log("Hitt")
-    return { success: true };
+    const responseType = 'code', clientId = 1657770559, redirect_uri='http://localhost:5173/callback'
+    const uri = `${linAutURL}?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirect_uri}&state=12345abcde&scope=openid%20email`
+    return {uri: uri}
   },
 };
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,15 @@
+<script lang="ts">
+  import { browser, building, dev, version } from '$app/environment';
+	import { goto } from '$app/navigation';
+  import type { PageData, ActionData } from './$types';
 
+  export let data: PageData;
+
+  export let form: ActionData;
+  let uri:string|undefined = form?.uri??'login'
+  console.log(browser)
+  if (browser) goto(uri);
+</script>
 <form method="POST" action="?/login">
     <button class="btn">line Login</button>
   </form>
